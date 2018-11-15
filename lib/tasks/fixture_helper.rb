805fd512b01ea77a59b87b96dee4b5b0c5c2bbb3
Luke Osborne <lwosborne@mitre.org>
2018-11-14 20:01:53 -0500

    fix backend_calc failing tests

--- lib/tasks/fixture_helper.rb
+++ lib/tasks/fixture_helper.rb
@@ -49,8 +49,6 @@ def insert_mongoid_ids(json)
       else
         insert_mongoid_ids(v)
       end
-    elsif %w[_id bundle_id user_id].include?(k)
-      json[k] = BSON::ObjectId.from_string(v)
     end
   end
 end
