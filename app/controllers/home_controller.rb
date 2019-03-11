e8ded4eb394cfcd39d608ee6e88266bccefbce05
daco101 <40396363+daco101@users.noreply.github.com>
2019-03-11 11:16:57 -0400

    Cqm parsers integration (#1179)
    * Add cqm measure conversion to measure helper
    
    * Convert to new measure model in bonnie_backend_calculator
    
    * Remove valuesetsbyoid as a required parameter by the backend calculator
    
    * Adjusting from old model CqlMeasure to CQM::Measure

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, :except => [:show]
 
   def index
-    @measures = Measure.by_user(current_user).only(:id) # Only using measure for JS URL generation, only need ID
-    @measures += CQM::Measure.where(user_id: current_user.id).only(:id) # Add in CQL measures
-    @measures += CqlMeasure.where(user_id: current_user.id).only(:id) # Add in CQL measures
+    @measures = CQM::Measure.by_user(current_user).only(:id)
     @patients = Record.by_user(current_user)
     @patients += QDM::Patient.by_user(current_user)
   end
