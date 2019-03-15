60b0e1e9a2552a48feff36182cb2cf72c74c775d
Luke Osborne <lwosborne@mitre.org>
2019-03-15 08:54:25 -0400

    Add extension to CQM::Patient in lib/ext/patient.rb

--- lib/ext/patient.rb
+++ lib/ext/patient.rb
@@ -1,10 +1,6 @@
-module QDM
+module CQM
   class Patient
-    field :measure_ids, type: Array
-   
     belongs_to :user
-    scope :by_measure_id, ->(id) { where({'measure_id'=>id }) }
     scope :by_user, ->(user) { where({'user_id'=>user.id}) }
-    index "user_id" => 1
   end
 end
