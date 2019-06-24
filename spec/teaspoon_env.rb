dbd95d0d8320aa83583e10f7a1adedaa4a6614f5
Luke Osborne <lwosborne@mitre.org>
2019-06-24 08:52:33 -0400

    adjust travis to debug failing tests

--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -216,7 +216,7 @@ Teaspoon.configure do |config|
   #
   # Available: :dot, :clean, :documentation, :json, :junit, :pride,
   #            :rspec_html, :snowday, :swayze_or_oprah, :tap, :tap_y, :teamcity
-  config.formatters = [:clean]
+  config.formatters = [:tap_y]
 
   # Specify if you want color output from the formatters.
   # config.color = true
